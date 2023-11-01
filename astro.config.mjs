eeaf61f25325242f321b1e0cddb2e4584b0e9e44
Ivan Castillo <45212708+castelll0009@users.noreply.github.com>
2023-10-31 23:04:43 -0400

    deploying

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    
+    site: 'https://castelll0009.github.io',
+    base: '/desk',
+});

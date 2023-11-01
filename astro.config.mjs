b28a4fecfee78928a6a3018260de05f499684b50
Ivan Castillo <45212708+castelll0009@users.noreply.github.com>
2023-11-01 02:22:23 -0400

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    
-    site: 'https://castelll0009.github.io',
-    base: '/desk',
+  site: 'https://castelll0009.github.io',
+  base: '/desk',
+  integrations: [tailwind()]
 });
